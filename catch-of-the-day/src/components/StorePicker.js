f0489c3cdfaac1d1607fb98f05f8bfcef7f0e3ef
Ognjen <opejovic@gmail.com>
2023-08-06 17:21:39 +0200

    11

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this);
+    console.log(this.myInput.current.value);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2> 
-        <input type="text" required placeholder="Store Name" />
+        <input ref={this.myInput} type="text" required placeholder="Store Name" />
         <button type="Submit">Visit Store</button>
       </form>
     );
