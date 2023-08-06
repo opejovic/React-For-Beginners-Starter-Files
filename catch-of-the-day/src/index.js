fd7e4a810424dd15375e6f44ef705841ebc5b4a6
Ognjen <opejovic@gmail.com>
2023-08-06 16:56:56 +0200

    9

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,6 @@
 // let's go!
 import { render } from 'react-dom';
-import App from './components/App';
+import Router from "./components/Router";
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
